af60bed650acdb681cf4c833a3e07c6bb45a5ded
David Achourbecove <kodziev@gmail.com>
2022-11-24 11:28:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            if(nums == null || nums.length == 0) return 1;
+            int n = nums.length;
+            int containsOne = 0;
+
+            for (int i = 0 ; i < n;i++){
+                if(nums[i] == 1){
+                    containsOne = 1;
+                }else if (nums[i] <= 0 || nums[i] > n){
+                    nums[i] = 1;
+                }
+            }
+            if (containsOne == 0) return 1;
+
+            //step 2
+            for(int i = 0 ; i < n ; i++){
+                int index = Math.abs(nums[i]) -1;
+                if (nums[index] > 0) nums[index] = -1 * nums[index];
+            }
+            //step3
+            for(int i = 0; i < n ;i++){
+                if(nums[i] > 0){return i+1;}
+            }
+            return n+1;
+        }
+    }
+
+
+}
